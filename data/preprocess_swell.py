ce581d925930d128a1570bfbaf026d7c364a696f
Paul Branson <paul.branson@csiro.au>
2022-08-03 16:18:40 -0700

    Additional features

--- data/preprocess_swell.py
+++ data/preprocess_swell.py
@@ -9,11 +9,17 @@ import xarray as xr
 
 def get_fset(fset):
     if fset == 0:
-        return ['hs','tp']
+        return ['hs','tpn']
     elif fset == 1:
-        return ['hs_u','hs_v','tp']
+        return ['hs_u','hs_v','tpn']
     elif fset == 2:
-        return ['hs','dp_u','dp_v','tp']
+        return ['hs','dp_u','dp_v','tpn']
+    elif fset == 3:
+        return ['hs','dp_u','dp_v','tpn','gamma']
+    elif fset == 4:
+        return ['hs','tpn','gamma']
+    elif fset == 5:
+        return ['hs','tpn','t_u','t_v']
 
 def preprocess(fset):
 
@@ -23,15 +29,22 @@ def preprocess(fset):
     outfile = f"swells/swells_rottnest_fset{fset}.npz"
     if not os.path.exists(outfile):
 
-        ds = xr.open_zarr('test_partitions_chunked_pmt3.zarr.zip',chunks='auto').isel(site=0)
+        ds = xr.open_zarr('test_partitions_pmt3.2.zarr.zip',chunks='auto').isel(site=0)
 
         basedate = pd.to_datetime(pd.to_datetime(ds.time[0].values).date())
         ds['time'] = (ds.time - basedate.asm8)/pd.to_timedelta(1,'d')
 
         ds_events = ds.stack(event=['time','part'])
         ds_events = ds_events.where(ds_events.hs > 0.1, drop=True)
-        ds_events['dp_rad'] = np.deg2rad(ds_events.dp + 7.5/2*np.random.randn(*ds_events.dp.shape))
-        ds_events['dp_u'], ds_events['dp_v'] = np.sin(ds_events.dp_rad), np.cos(ds_events.dp_rad),
+
+        maxT = 28.571428298950195
+        ds_events['dpn'] = ds_events.dp + 15*0.367*(np.random.randn(*ds_events.dp.shape))
+        ds_events['dpn'] = ds_events['dpn'] % 360.
+        ds_events['tpn'] = ds_events.tp + ds_events.tp/maxT*(np.random.randn(*ds_events.tp.shape))
+        ds_events['gamma'] = ds_events.gamma.where(ds_events.gamma > 1, ds_events.gamma + 0.05*np.random.randn(*ds_events.gamma.shape))
+
+        ds_events['dpn_rad'] = np.deg2rad(ds_events.dpn)
+        ds_events['dp_u'], ds_events['dp_v'] = np.sin(ds_events.dpn_rad), np.cos(ds_events.dpn_rad),
         ds_events['hs_u'], ds_events['hs_v'] = ds_events.hs * ds_events.dp_u, ds_events.hs * ds_events.dp_v
 
         ds_features = ds_events.reset_index('event').reset_coords()[['time',] + features]
@@ -39,6 +52,8 @@ def preprocess(fset):
         df = ds_features.to_pandas().reset_index().drop(columns='event')
 
         df = df.set_index('time')
+        df['t_u'] = np.sin(df.index.day_of_year/366*2.*np.pi)
+        df['t_v'] = np.cos(df.index.day_of_year/366*2.*np.pi)
         print(len(df))
         df = df.dropna()
         print(len(df))
@@ -60,6 +75,7 @@ def preprocess(fset):
             sort_inds = t.argsort()
             t = t[sort_inds]
             x = df_range.to_numpy().astype(np.float64)[sort_inds]
+
             print(len(t))
 
             sequences[seq_name] = np.concatenate([t[:,None],x],axis=1)
